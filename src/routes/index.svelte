5dad358a0a1a8b15e8100c4e5e08012326fd0578
Peter Duerst <peterad12@gmail.com>
2022-07-01 17:26:00 -0400

    Added About page and Header

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
+<svelte:head>
+	<title>Prismatic Dragon Generator</title>
+	<meta name="description" content="Stat block generator for the prismatic dragons" />
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
